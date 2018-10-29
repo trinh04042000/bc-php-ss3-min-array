7fc6799820d82a6675e13134168114b027c34758
trinh04042000 <letrinh04042000@gmail.com>
2018-10-29 14:07:53 +0700

    add new file

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+  function findMinScore($arr)
+  {
+      $min = $arr[0];
+      for ($i = 1; $i < count($arr); $i++) {
+          if ($arr[$i] < $min) {
+              $min = $arr[$i];
+          }
+      }
+      return $min;
+  }
+  $arr = [2,4,-3,5,1,7,8,-56];
+  echo(findMinScore($arr));
+  ?>
+</body>
+</html>
